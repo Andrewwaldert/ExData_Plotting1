4de23ccbe2f3da0c20bed2cebd20433c22c98a7c
Andrewwaldert <andreas.waldert@cass.city.ac.uk>
2016-03-31 11:12:47 +0200

    Final Version Plot2

--- plot2.R
+++ plot2.R
@@ -14,5 +14,50 @@
 ## Checking the head of the data 
 > head(Usable_Data)
 
-Opening Quartz
-> quarts()
+            Date     Time Global_active_power Global_reactive_power
+66637 2007-02-01 00:00:00               0.326                 0.128
+66638 2007-02-01 00:01:00               0.326                 0.130
+66639 2007-02-01 00:02:00               0.324                 0.132
+66640 2007-02-01 00:03:00               0.324                 0.134
+66641 2007-02-01 00:04:00               0.322                 0.130
+66642 2007-02-01 00:05:00               0.320                 0.126
+      Voltage Global_intensity Sub_metering_1 Sub_metering_2
+66637 243.150            1.400          0.000          0.000
+66638 243.320            1.400          0.000          0.000
+66639 243.510            1.400          0.000          0.000
+66640 243.900            1.400          0.000          0.000
+66641 243.160            1.400          0.000          0.000
+66642 242.290            1.400          0.000          0.000
+      Sub_metering_3
+66637              0
+66638              0
+66639              0
+66640              0
+66641              0
+66642              0
+
+
+## Creating a new posix date 
+> Usable_Data$posix <- as.POSIXct(strptime(paste(Usable_Data$Date, Usable_Data$Time, sep = " "), format = "%Y-%m-%d %H:%M:%S"))
+
+## Checking the Class of Usable_Data$Global_active_power
+> class(Usable_Data$Global_active_power)
+[1] "factor"
+
+##as.numeric
+> Usable_Data$Global_active_power <- as.numeric(Usable_Data$Global_active_power)
+
+## creating plot2.png
+> png(file = "plot2.png", width = 480, height = 480, units = "px")
+> with(Usable_Data, plot(posix, Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power (in Kilowatts)"))
+
+## restarting Quartz
+> dev.off()
+quartz 
+     2 
+> quartz()
+
+## Plotting again
+> with(Usable_Data, plot(posix, Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power (in Kilowatts)"))
+
+
